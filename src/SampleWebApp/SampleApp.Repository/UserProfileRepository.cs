b3cf00c413c1d8bda00a73f6de7071794da8f8c3
nick <eiu165@gmail.com>
2013-05-17 21:24:26 -0500

    moved files into separate layers

---
+++ src/SampleWebApp/SampleApp.Repository/UserProfileRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Globalization;
+
+namespace SampleWebApp.Models
+{
+	public class UsersContext : DbContext
+	{
+		public UsersContext()
+			: base("DefaultConnection")
+		{
+		}
+
+		public DbSet<UserProfile> UserProfiles { get; set; }
+	}
+
+	[Table("UserProfile")]
+	public class UserProfile
+	{
+		[Key]
+		[DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+		public int UserId { get; set; }
+		public string UserName { get; set; }
+	}
+
+
+
+}
